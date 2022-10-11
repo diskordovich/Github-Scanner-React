e89ce384f20295e462d01a396a651d4affe03b38
diskordovich <diskordovich@gmail.com>
2022-10-11 19:25:09 +0300

    Adds routing

--- github-reader/src/App.js
+++ github-reader/src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Route, Routes, Link} from "react-router-dom"
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Routes>
+        <Route path='/' element={<div>Home</div>}></Route>
+        <Route path='/:id' element={<div>Id page</div>}></Route>
+      </Routes>
     );
   }
 }
